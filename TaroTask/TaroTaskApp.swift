474ad7dd7299af9363ce73f140cf99dee7039288
Joyce <s022466@students.lmsd.org>
2025-02-21 12:43:10 -0500

    Initial Commit

---
+++ TaroTask/TaroTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaroTaskApp.swift
+//  TaroTask
+//
+//  Created by Joyce Chang (student LM) on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct TaroTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
