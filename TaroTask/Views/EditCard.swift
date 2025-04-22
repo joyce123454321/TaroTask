5a1d73692ae7c4e61366e67f2523af2968dab382
Joyce <s022466@students.lmsd.org>
2025-04-22 12:47:04 -0400

    nom

---
+++ TaroTask/Views/EditCard.swift
@@ -0,0 +1,18 @@
+//
+//  EditCard.swift
+//  TaroTask
+//
+//  Created by Joyce Chang (student LM) on 3/24/25.
+//
+
+import SwiftUI
+
+struct EditCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCard()
+}
